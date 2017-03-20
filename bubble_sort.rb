2e4a8221e56abe85e5356e0ecdfb28f55b56debd
rwilhel <rwilhel@gmail.com>
2017-03-19 18:02:33 -0600

    "Edits to bubble_sort.rb"

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,22 @@
 class BubbleSort
-  def bubble_sort array
-      n = array.length
-  loop do 
-      swapped = false
-
-      (n-1).times do |i|
-      if array[i] > array [i+1]
-        array[i], array[i+1] = array[i+1], array[i]
-      swapped = true
-      end
-  do
   
-  break if not swapped 
+  def sort(set)
+    swapped = false
+    n = set.length
+    while swapped == false do
+      swapped = true
+      (n - 1).times do |i|
+        if set[i] > set[i + 1]
+         set[i], set[i + 1] = set[i + 1], set[i]
+        swapped = false  
+      end
+     end
+    end
+    puts set
+  end
 end
 
-  array
-
-end
-
-a = ["d", "b", "a", "c"]
-p bubble_sort(a)
+letters = ["d", "b", "a", "c"]
+sorter = BubbleSort.new
+sorter.sort(letters)
 
